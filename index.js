9db4ee406c034aa4d7a682a553fe24abb1eb79e4
islamMaruf <marufislam7424@gmail.com>
2020-04-25 19:35:14 +0600

    create block chain

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const sha256 =  require('crypto-js/sha256');
+const sha256 = require('crypto-js/sha256');
+
 class Block {
-    constructor(data, previousHash) {
+    constructor(data, previousHash = null) {
         this.data = data;
         this.previousHash = previousHash;
         this.timestamps = Date.now();
         this.hash = this.calculateHash();
     }
-    calculateHash(){
+
+    calculateHash() {
         return sha256(
             this.timestamps + JSON.stringify(this.data) + this.previousHash
         ).toString()
     }
 }
-class BlockChain{
+
+class BlockChain {
     constructor() {
-        this.chain = [];
+        this.chain = [this.generateGenesisBlock()];
     }
-    addBlock(newBlock){
+    generateGenesisBlock(){
+        return new Block("GENSIS","00000");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
-   
+
 }
 
 
-let data = {name:'maruf',age:56};
-let block = new Block(data,null);
+let data = {name: 'maruf', age: 56};
+let block = new Block(data);
 let jossCoin = new BlockChain();
 jossCoin.addBlock(block);
+jossCoin.addBlock(new Block({name: 'maruf islam', 'age': 56}))
+console.log(jossCoin);
